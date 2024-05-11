540d77931abfbb90c8b697c369f8383da7e7ad31
LukeisStuff <lukeisawesome03@gmail.com>
2024-05-11 11:32:29 -0400

    apple saplings generate in labyrinths and dungeons

---
+++ src/main/java/luke/stardew/mixin/WorldFeatureLabyrinthMixin.java
@@ -0,0 +1,21 @@
+package luke.stardew.mixin;
+
+import luke.stardew.blocks.StardewBlocks;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.generate.feature.WorldFeatureLabyrinth;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(value = WorldFeatureLabyrinth.class, remap = false)
+public class WorldFeatureLabyrinthMixin {
+	@Inject(method = "pickCheckLootItem(Ljava/util/Random;)Lnet/minecraft/core/item/ItemStack;", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"), cancellable = true)
+	private void addLoot(Random random, CallbackInfoReturnable<ItemStack> cir){
+		if (random.nextInt(16) == 0 && random.nextInt(10) == 0){
+			cir.setReturnValue(new ItemStack(StardewBlocks.saplingApple, random.nextInt(1) + 1));
+		}
+	}
+}
