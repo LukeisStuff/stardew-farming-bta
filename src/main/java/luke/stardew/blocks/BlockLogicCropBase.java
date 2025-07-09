ca48dc97ec2b93c04c54c421ffdfb1a267cdeec5
LukeisStuff <lukeisawesome03@gmail.com>
2025-07-09 03:05:24 -0400

    fix trellis lang, change it so seeds arent dropped from crops you can right click harvest

--- src/main/java/luke/stardew/blocks/BlockLogicCropBase.java
+++ src/main/java/luke/stardew/blocks/BlockLogicCropBase.java
@@ -84,9 +84,8 @@ public class BlockLogicCropBase extends BlockLogicFlower implements IBonemealabl
 		return this;
 	}
 
-	public BlockLogicCropBase noHarvest() {
+	public void noHarvest() {
 		this.canHarvest = false;
-		return this;
 	}
 
 	@Override
@@ -230,7 +229,6 @@ public class BlockLogicCropBase extends BlockLogicFlower implements IBonemealabl
 			world.playSoundEffect(player, SoundCategory.WORLD_SOUNDS, (double)x + 0.5, (double)y + 0.5, (double)z + 0.5, "random.pop", 0.3F, 1.0f);
 			if (!world.isClientSide) {
 				if(this.cropItem != null) world.dropItem(x, y, z, new ItemStack(this.cropItem, this.cropRange.get(world.rand)));
-				if(this.seedItem != null) world.dropItem(x, y, z, new ItemStack(this.seedItem, this.seedRange.get(world.rand)));
 			}
 			return true;
 		}
