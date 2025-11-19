154f2eda94c12dc4de3a17e5f3735d9dfc882a35
LukeisStuff <lukeisawesome03@gmail.com>
2025-11-18 20:17:08 -0500

    cleanup cauliflowwer and melon code

--- src/main/java/luke/stardew/WorldFeatureCauliflower.java
+++ src/main/java/luke/stardew/WorldFeatureCauliflower.java
@@ -3,6 +3,7 @@ package luke.stardew;
 import luke.stardew.blocks.StardewBlocks;
 import net.minecraft.core.block.Blocks;
 import net.minecraft.core.world.World;
+import net.minecraft.core.world.biome.Biome;
 import net.minecraft.core.world.biome.Biomes;
 import net.minecraft.core.world.generate.feature.WorldFeature;
 
@@ -12,26 +13,19 @@ public class WorldFeatureCauliflower extends WorldFeature {
 
 	@Override
 	public boolean place(World world, Random random, int x, int y, int z) {
+		Biome biome = world.getBlockBiome(x, y, z);
+
+		if (!(biome == Biomes.OVERWORLD_TUNDRA || biome == Biomes.OVERWORLD_BOREAL_FOREST || biome == Biomes.OVERWORLD_TAIGA || biome == Biomes.OVERWORLD_GLACIER)) {
+			return false;
+		}
+
 		for (int l = 0; l < 64; ++l) {
 			int i1 = x + random.nextInt(8) - random.nextInt(8);
 			int j1 = y + random.nextInt(4) - random.nextInt(4);
 			int k1 = z + random.nextInt(8) - random.nextInt(8);
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_TUNDRA && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.CAULIFLOWER.canPlaceBlockAt(world, i1, j1, k1)) {
+			if (world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.CAULIFLOWER.canPlaceBlockAt(world, i1, j1, k1)) {
 				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.CAULIFLOWER.id());
 			}
-
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_BOREAL_FOREST && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.CAULIFLOWER.canPlaceBlockAt(world, i1, j1, k1)) {
-				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.CAULIFLOWER.id());
-			}
-
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_TAIGA && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.CAULIFLOWER.canPlaceBlockAt(world, i1, j1, k1)) {
-				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.CAULIFLOWER.id());
-			}
-
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_GLACIER && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.CAULIFLOWER.canPlaceBlockAt(world, i1, j1, k1)) {
-				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.CAULIFLOWER.id());
-			}
-
 		}
 		return true;
 	}
--- src/main/java/luke/stardew/WorldFeatureMelon.java
+++ src/main/java/luke/stardew/WorldFeatureMelon.java
@@ -3,6 +3,7 @@ package luke.stardew;
 import luke.stardew.blocks.StardewBlocks;
 import net.minecraft.core.block.Blocks;
 import net.minecraft.core.world.World;
+import net.minecraft.core.world.biome.Biome;
 import net.minecraft.core.world.biome.Biomes;
 import net.minecraft.core.world.generate.feature.WorldFeature;
 
@@ -12,30 +13,19 @@ public class WorldFeatureMelon extends WorldFeature {
 
 	@Override
 	public boolean place(World world, Random random, int x, int y, int z) {
-		for (int l = 0; l < 64; ++l) {
+		Biome biome = world.getBlockBiome(x, y, z);
+
+		if (!(biome == Biomes.OVERWORLD_CAATINGA_PLAINS || biome == Biomes.OVERWORLD_CAATINGA || biome == Biomes.OVERWORLD_RAINFOREST || biome == Biomes.OVERWORLD_SWAMPLAND || biome == Biomes.OVERWORLD_SWAMPLAND_MUDDY)) {
+			return false;
+		}
+
+		for (int l = 0; l < 64; l++) {
 			int i1 = x + random.nextInt(8) - random.nextInt(8);
 			int j1 = y + random.nextInt(4) - random.nextInt(4);
 			int k1 = z + random.nextInt(8) - random.nextInt(8);
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_CAATINGA && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.WATERMELON.canPlaceBlockAt(world, i1, j1, k1)) {
+			if (world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, k1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.WATERMELON.canPlaceBlockAt(world, i1, j1, k1)) {
 				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.WATERMELON.id());
 			}
-
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_CAATINGA_PLAINS && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.WATERMELON.canPlaceBlockAt(world, i1, j1, k1)) {
-				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.WATERMELON.id());
-			}
-
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_RAINFOREST && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.WATERMELON.canPlaceBlockAt(world, i1, j1, k1)) {
-				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.WATERMELON.id());
-			}
-
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_SWAMPLAND_MUDDY && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.WATERMELON.canPlaceBlockAt(world, i1, j1, k1)) {
-				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.WATERMELON.id());
-			}
-
-			if (world.getBlockBiome(x, y, z) == Biomes.OVERWORLD_SWAMPLAND && world.isAirBlock(i1, j1, k1) && world.getBlockId(i1, j1 - 1, k1) == Blocks.GRASS.id() && StardewBlocks.WATERMELON.canPlaceBlockAt(world, i1, j1, k1)) {
-				world.setBlockWithNotify(i1, j1, k1, StardewBlocks.WATERMELON.id());
-			}
-
 		}
 		return true;
 	}
